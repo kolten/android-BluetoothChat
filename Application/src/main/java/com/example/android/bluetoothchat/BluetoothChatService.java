2a919bde7d4095f1292614f387d46dd1e35eda33
Kolten Sturgill <koltensturgill92@gmail.com>
2019-01-24 20:43:46 -0600

    updated UUID to enable arduino bluetooth module to connect

--- Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatService.java
+++ Application/src/main/java/com/example/android/bluetoothchat/BluetoothChatService.java
@@ -47,10 +47,12 @@ public class BluetoothChatService {
     private static final String NAME_INSECURE = "BluetoothChatInsecure";
 
     // Unique UUID for this application
+    // Buried in the internet you have to change two lines to make this example work
+    // http://android-er.blogspot.com/2016/04/android-bluetoothchat-example-link-with.html
     private static final UUID MY_UUID_SECURE =
-            UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
+            UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
     private static final UUID MY_UUID_INSECURE =
-            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+            UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     // Member fields
     private final BluetoothAdapter mAdapter;
